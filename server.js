fcf00989b373e38903a6ecd5fdc8ada52af947d9
Artem Karpovich <karpartem94@gmail.com>
2017-05-16 16:05:20 +0300

    add cors

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const faker = require('faker');
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
+
 app.get('/users', (req, res) => {
   const users = [];
 
-  for (let i = 0; i <= 2000; i++) {
+  for (let i = 0; i <= 1999; i++) {
     const user = {
       firstName: faker.name.firstName(),
       lastName: faker.name.lastName(),

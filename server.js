d7fbb0736922a07611ea233a55e4db930a31aee5
Artem Karpovich <karpartem94@gmail.com>
2017-05-16 19:50:03 +0300

    filter users

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const faker = require('faker');
+const uuidV1 = require('uuid/v1');
 
 const app = express();
 
@@ -9,20 +10,24 @@ app.use(cors({
   credentials: true,
 }));
 
+const users = [];
+
+for (let i = 0; i <= 1999; i++) {
+  const user = {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    country: faker.address.country(),
+    id: uuidV1(),
+  };
+
+  users.push(user);
+}
+
+
 app.get('/users', (req, res) => {
-  const users = [];
+  const { perPage, page } =  req.query;
 
-  for (let i = 0; i <= 1999; i++) {
-    const user = {
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      country: faker.address.country(),
-    };
-
-    users.push(user);
-  }
-
-  res.json(users);
+  res.json(users.slice(perPage * (page - 1), perPage * (page - 1) + perPage));
 });
 
 process.on('ungandledRejection', r => logger.error('Unhandled rejection', r));
